7b9584fc4d9678a1948d510999cee2b366bef10a
Mari Afonso <marianaafonso007@gmail.com>
2024-04-21 21:14:02 -0300

    Construção do programa que realiza a cópia e criação de um arquivo, com tratativa de erros ! :)

---
+++ ManipulandoArquivo/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Olar, seja bem vindo ao programa que realiza a cópia entre dois arquivos! :)");
+Console.WriteLine("Digite o nome do arquivo de origem:");
+string arquivoOrigem = Console.ReadLine();
+
+Console.WriteLine("Digite o nome do arquivo de destino:");
+string arquivoDestino = Console.ReadLine();
+
+CopiaArquivos(arquivoOrigem, arquivoDestino);
+ 
+ 
+ static void CopiaArquivos(string origem, string destino)
+    {
+        try
+        {
+            if (!File.Exists(origem))
+            {
+                Console.WriteLine($"O arquivo de origem '{origem}' não existe.");
+                return;
+            }
+            if (File.Exists(destino))
+            {
+                Console.WriteLine($"O arquivo de destino '{destino}' já existe.");
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader(origem))
+            {
+                using (StreamWriter sw = new StreamWriter(destino))
+                {
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        sw.WriteLine(linha);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Conteúdo de '{origem}' copiado para '{destino}' com sucesso.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ocorreu um erro durante a cópia: {e.Message}");
+        }
+    }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ManipulandoArquivo
 Da série de desafios do Desmistificando a Linguagem de Programação C# - Resolução do desafio Manipulando Arquivos :)
+
+![alt text](image.png)
